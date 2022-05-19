8d21802dd0b26d2be9943b6c408a2c43f0968acd
Chris Sisk <dogsrule_2003@yahoo.com>
2022-05-18 20:54:30 -0700

    initial commit

---
+++ src/main/java/com/codingdojo/daikichipathvariables/ChrisSiskDaikichiPathVariablesApplication.java
@@ -0,0 +1,13 @@
+package com.codingdojo.daikichipathvariables;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChrisSiskDaikichiPathVariablesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChrisSiskDaikichiPathVariablesApplication.class, args);
+	}
+
+}
